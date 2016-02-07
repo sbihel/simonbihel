c6c8439f44d1da378cea1d0ff939a4896bd351e2
Simon Bihel <simon.bihel@outlook.com>
2016-02-07 22:34:20 +0100

    Try fix wsgi import error

--- simonbihel/wsgi.py
+++ simonbihel/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simonbihel.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simonbihel.com.settings")
 
 application = get_wsgi_application()
