19ed19f80a3d6f70e4ddf32d7d1f09008cd643a6
Simon Bihel <simon.bihel@outlook.com>
2016-02-08 20:52:44 +0100

    Seems to work, now just need to add content Kappa

--- simonbihel/urls.py
+++ simonbihel/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^common/', include('common.urls')),
+    url(r'^', include('common.urls')),
     url(r'^admin/', admin.site.urls),
 ]
